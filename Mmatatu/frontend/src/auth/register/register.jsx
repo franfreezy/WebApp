e00c848e5b8d70fb34678c418771d11d6da34db1
franfreezy <frandelwanjawa@yahoo.com>
2024-11-07 08:42:47 +0300

    fix registration route

--- Mmatatu/frontend/src/auth/register/register.jsx
+++ Mmatatu/frontend/src/auth/register/register.jsx
@@ -28,7 +28,7 @@ function Register() {
 
     try {
       const response = await fetch(
-        "https://agroxsat.onrender.com/backend/register/",
+        "https://matatuback.onrender.com/backend/register/",
         {
           method: "POST",
           headers: {
