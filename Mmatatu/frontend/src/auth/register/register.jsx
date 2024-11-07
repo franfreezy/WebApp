d8a8807b3a79a93533e582988b12c875d20df541
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-11-07 12:42:32 +0300

    Push

--- Mmatatu/frontend/src/auth/register/register.jsx
+++ Mmatatu/frontend/src/auth/register/register.jsx
@@ -73,6 +73,24 @@ function Register() {
             <form class="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label
+                  for="username"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your Username
+                </label>
+                <input
+                  type="email"
+                  name="username"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="John Doe"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
                   for="email"
                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
@@ -120,7 +138,6 @@ function Register() {
                   id="confirm-password"
                   value={confirmPassword}
                   onChange={(e) => setConfirmPassword(e.target.value)}
-                
                   placeholder="••••••••"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required=""
@@ -240,6 +257,6 @@ function Register() {
       </div>
     </section>
   );
-};
+}
 
 export default Register;
