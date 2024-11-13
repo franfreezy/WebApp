9b9f7b4d8789d1c7998465f1359181b402746f24
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-11-13 10:10:09 +0300

    Push the login username thing

--- Mmatatu/frontend/src/auth/login/login.jsx
+++ Mmatatu/frontend/src/auth/login/login.jsx
@@ -3,7 +3,7 @@ import { Navigate, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [formData, setFormData] = useState({
-    email: "",
+    username: "",
     password: "",
   });
 
@@ -92,8 +92,8 @@ const Login = () => {
                   type="username"
                   name="username"
                   id="username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
+                  value={formData.username}
+                  onChange={handleChange}
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="John Doe"
                   required=""
