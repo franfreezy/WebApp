67db694124f6b12b20d2df7f72583b0816cb2443
franfreezy <frandelwanjawa@yahoo.com>
2024-11-16 10:53:31 +0300

    FIx logout

--- Mmatatu/frontend/src/dashboard/components/topbar.jsx
+++ Mmatatu/frontend/src/dashboard/components/topbar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 const TopBar = () => {
+  const handleSignOut = () => {
+    localStorage.removeItem('accessToken');
+  localStorage.removeItem('refreshToken');
+  localStorage.removeItem('username');
+  localStorage.removeItem('email');
+
+  
+  window.location.href = '/';
+  };
   return (
     <div className="bg-white shadow-md flex items-center justify-between px-6 py-3">
       <div className="text-xl font-semibold text-gray-800">Dashboard</div>
       <div className="flex items-center space-x-4">
         <button className="text-gray-800 hover:text-blue-500">Profile</button>
-        <button className="text-gray-800 hover:text-blue-500">Logout</button>
+        <button onClick={handleSignOut} className="text-gray-800 hover:text-blue-500">Logout</button>
       </div>
     </div>
   );
