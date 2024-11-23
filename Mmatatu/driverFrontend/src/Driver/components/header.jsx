cb5218e1e9945b63c6b0c11e2c57ec69dfd83273
franfreezy <frandelwanjawa@yahoo.com>
2024-11-23 12:19:13 +0300

    Configure sign out

--- Mmatatu/driverFrontend/src/Driver/components/header.jsx
+++ Mmatatu/driverFrontend/src/Driver/components/header.jsx
@@ -1,4 +1,13 @@
 export default function Header() {
+  const handleSignOut = () => {
+    localStorage.removeItem('accessToken');
+  localStorage.removeItem('refreshToken');
+  localStorage.removeItem('username');
+  localStorage.removeItem('email');
+
+  
+  window.location.href = '/';
+  };
   return (
     <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
       <div className="flex items-center space-x-3">
@@ -9,7 +18,7 @@ export default function Header() {
         />
         <h1 className="text-lg font-semibold">Driver Name</h1>
       </div>
-      <button className="bg-red-500 px-4 py-2 rounded">Logout</button>
+      <button  onClick={handleSignOut} className="bg-red-500 px-4 py-2 rounded">Logout</button>
     </header>
   );
 }
