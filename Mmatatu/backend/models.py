0a03bb2a3daea616c13c4ca599a974408d7b5014
franfreezy <frandelwanjawa@yahoo.com>
2024-11-23 12:28:20 +0300

    Fare model configured

--- Mmatatu/backend/models.py
+++ Mmatatu/backend/models.py
@@ -8,85 +8,10 @@ class Coordinates(models.Model):
     def __str__(self):
         return f"Lat: {self.latitude}, Long: {self.longitude}"
 
-class Images(models.Model):
-    image = models.TextField()
-    image_name = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
-
-    def __str__(self):
-        return f"Temp: {self.image}, Time: {self.timestamp}"
-    
-class GSCoordinates(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+class Fare(models.Model):
+    Route = models.CharField()
+    Rate = models.FloatField()
     updated_at = models.DateTimeField(auto_now=True)
-    
-    def coordinatesave(self, *args, **kwargs):
-        # Before saving a new value, delete the previous value
-        GSCoordinates.objects.all().delete()
-        # super(GSCoordinates, self).save(*args, **kwargs)
-        
-    def __str__(self):
-        return f"Lat: {self.latitude}, Long: {self.longitude}"    
-
-class batt(models.Model):
-    batt = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
 
     def __str__(self):
-        return f"batt: {self.batt}, Time: {self.timestamp}"
-
-class humidity(models.Model):
-    humidity = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
-
-    def __str__(self):
-        return f"batt: {self.batt}, Time: {self.timestamp}"
-    
-class temperature(models.Model):
-    temperature = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
-
-    def __str__(self):
-        return f"batt: {self.batt}, Time: {self.timestamp}"
-class smoke(models.Model):
-    smoke = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
-
-    def __str__(self):
-        return f"batt: {self.batt}, Time: {self.timestamp}"
-class soilph(models.Model):
-    soilph = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
-
-    def __str__(self):
-        return f"batt: {self.batt}, Time: {self.timestamp}"
-class soilprecipitation(models.Model):
-    soilprecipitation = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
-
-    def __str__(self):
-        return f"batt: {self.batt}, Time: {self.timestamp}"
-class location(models.Model):
-    location = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
-
-    def __str__(self):
-        return f"batt: {self.batt}, Time: {self.timestamp}"
-    
-class gsm_coords(models.Model):
-    latitude = models.FloatField()  
-    longitude = models.FloatField()  
-    timestamp = models.DateTimeField(auto_now_add=True)
-     
-
-    def __str__(self):
-        return self.name
+        return f"Lat: {self.Route}, Long: {self.Rate}"
